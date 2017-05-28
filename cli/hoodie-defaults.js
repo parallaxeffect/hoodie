2af695cc367b931f878b2ba92404668b65be872f
La Baik <labaik237@gmail.com>
2017-05-28 18:32:16 +0200

    feat: 3rd party plugins

--- cli/hoodie-defaults.js
+++ cli/hoodie-defaults.js
@@ -2,6 +2,7 @@ module.exports = getHoodieDefaults
 
 function getHoodieDefaults () {
   return {
+    name: undefined,
     address: '127.0.0.1',
     port: 8080,
     data: '.hoodie',
@@ -13,6 +14,8 @@ function getHoodieDefaults () {
     inMemory: false,
     loglevel: 'warn',
     url: undefined,
-    adminPassword: undefined
+    adminPassword: undefined,
+    plugins: [],
+    app: {}
   }
 }
